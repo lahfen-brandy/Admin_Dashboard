2a45d50d380b9f199b7fab4f9f7b292a120d4fbc
lahfenbrandyf <lahfenbrandy@gmail.com>
2025-05-14 19:36:47 +0100

    fourth commit

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,63 @@ var sideBaropen = false;
 var sideBar = document.getElementById("sideBar");
 
 function openSidebar() {
-    if(!sideBaropen) {
+    if (!sideBaropen) {
         sideBar.classList.add("sidebar-responsive");
         sideBaropen = true;
     }
 }
 
-function closeSidebar(){
-    if(sideBaropen){
+function closeSidebar() {
+    if (sideBaropen) {
         sideBar.classList.remove("sidebar-responsive")
         sideBaropen = false;
     }
 }
+
+// Apex Horizontal Bar Chart
+
+var barChartOptions = {
+    series: [{
+        data: [10, 8, 6, 4, 2]
+    }],
+    chart: {
+        type: 'bar',
+        height: 350,
+        toolbar: {
+            show: false
+        },
+    },
+    colors: [
+        "#246dec",
+        "#cc3c43",
+        "#367952",
+        "#f5b74f",
+        "#4f35a1"
+    ],
+    plotOptions: {
+        bar: {
+            distributed: true,
+            borderRadius: 4,
+            borderRadiusApplication: 'end',
+            horizontal: false,
+            columnWidth: '40%',
+        }
+    },
+    dataLabels: {
+        enabled: false
+    },
+    legend: {
+        show: false
+    },
+    xaxis: {
+        categories: ["Laptop", "Phone", "Monitor", "Headphones", "Camera"],
+    },
+    yaxis: {
+        title: {
+            text: "Count"
+        }
+    }
+};
+
+var barchart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
+barchart.render();
