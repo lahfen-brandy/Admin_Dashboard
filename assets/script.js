c3eaf11060ac629fcb750cb000c7f77f51615ef7
lahfenbrandyf <lahfenbrandy@gmail.com>
2025-05-15 15:46:49 +0100

    fourth commit

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+
+
 // SideBar Toggle
 
 var sideBaropen = false;
@@ -17,7 +19,9 @@ function closeSidebar() {
     }
 }
 
-// Apex Horizontal Bar Chart
+
+
+// Apex Vertical Bar Chart
 
 var barChartOptions = {
     series: [{
@@ -64,3 +68,60 @@ var barChartOptions = {
 
 var barchart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
 barchart.render();
+
+
+
+
+// AREA CHART
+
+var areaChartOptions = {
+    series: [{
+        name: 'Purchase Orders',
+        data: [31, 40, 28, 51, 42, 109, 100]
+    }, {
+        name: 'Sales Orders',
+        data: [11, 32, 45, 32, 34, 52, 41]
+    }],
+    chart: {
+        height: 350,
+        type: 'area',
+        toolbar: {
+            show: false,
+        }
+    },
+
+    colors: ["#4f35a1", "#246dec"],
+
+    dataLabels: {
+        enabled: false,
+    },
+
+    stroke: {
+        curve: 'smooth'
+    },
+
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+    markers: {
+        size: 0
+    },
+    yaxis: [
+        {
+            title: {
+                text: 'Purchase Orders',
+            },
+        },
+        {
+            opposite: true,
+            title: {
+                text: 'Sales Orders',
+            },
+        },
+    ],
+    tooltip: {
+        shared: true,
+        intersect: false,
+    }
+};
+
+var areachart = new ApexCharts(document.querySelector("#area-chart"), areaChartOptions);
+areachart.render();
